267c894d1b5edb978e762cd3f54176921f91b567
Cartodream <90085887+Cartodream@users.noreply.github.com>
2021-11-08 15:08:24 +0100

    Add files via upload

---
+++ donnees/POI.js
@@ -0,0 +1,14 @@
+var poi = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+
+
+{ "type": "Feature", "properties": { "id": "ARTBOU0210021133", "nom": "Eglise de Fayet", "descriptif": null, "tel": null, "mail": null, "site_web": "", "sous_cat": "Patrimoine religieux", "categorie": "Patrimoine culturel", "adresse": null, "adresse2": "", "cp": null, "commune": null, "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [ 3.536548614629525, 45.42470313081263  ] } },
+
+
+
+{ "geometry": {"type": "Point", "coordinates": [3.536040889670831,45.42625077109817]},"type": "Feature", "properties": {"nom": "Mairie", "descriptif" : "Heures d'ouverture : le mardi de 14h à 17h30, le Mercredi de 9h à 11h30 et le Jeudi de 9h à 12h ", "tel": "33 (0)4.73.72.04.43", "commune": null, "photo": null, "sous_cat": "Mairie", "adresse": "Le Bourg" , "categorie": "Bâtiment Public", "source": null, "site_web": null, "mail": "", "cp": "63630", "id": 245}}
+]
+};
